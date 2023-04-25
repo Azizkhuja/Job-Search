89c03ba38a5f91ce0625005fcc6dfbd425d12cd1
Aziz <aziz.khadjayev070@gmail.com>
2023-04-25 19:09:07 +0500

    Add router and state

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
 const Home = () => {
   return (
