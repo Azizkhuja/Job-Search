deb4aff6fbd12cbfac3063e83f625055ad6fa693
Aziz <aziz.khadjayev070@gmail.com>
2023-04-26 16:11:31 +0500

    Import hooks

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,9 @@
-const { Stack } = require("expo-router");
+import { useCallback } from "react";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
   return <Stack />;
