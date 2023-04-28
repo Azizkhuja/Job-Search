2b5038d30b45b8b5206cbaf653221d0f4390399f
Aziz <aziz.khadjayev070@gmail.com>
2023-04-28 16:37:56 +0500

    Create show-all component

---
+++ app/show-all/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  Image,
+  View,
+  FlatList,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
+import { checkImageURL } from "../../utils";
+import { useFetch } from "../../hook/useFetch";
+import axios from "axios";
+
+const ShowAll = () => {};
+
+export default ShowAll;
