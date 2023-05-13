808ab2fd1f39657da02ed2b925eab4af97e09aee
Aziz <aziz.khadjayev070@gmail.com>
2023-05-13 09:13:12 +0500

    remove onboard component

--- app/Onboarding.jsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "react-native";
-
-const Onboarding = ({ handleSignIn }) => {
-  return <Button title="Login with Google" onPress={handleSignIn} />;
-};
-
-export default Onboarding;
