a43a514383d0bbbcf7739d1a55d650bdfafb079b
Aziz <aziz.khadjayev070@gmail.com>
2023-05-14 11:04:31 +0500

    Wrapper button

--- app/onboarding/onBoarding.style.js
+++ app/onboarding/onBoarding.style.js
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  googleBtn: {
+    width: 184,
+    height: 42,
+    backgroundColor: "#4285f4",
+    borderRadius: 2,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
 
 export default styles;
