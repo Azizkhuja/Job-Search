86b65b4a8adeab1cc6d390c67e466087b17a2f2c
Aziz <aziz.khadjayev070@gmail.com>
2023-05-16 10:08:39 +0500

    Create Onboarding view screen

---
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -0,0 +1,10 @@
+import { TouchableOpacity, View, Image, Text } from "react-native";
+
+
+const OnboardingViewScreen = ({ handleSignIn }) => {
+  return (
+    
+  );
+};
+
+export default OnboardingViewScreen;
