90f2dc41f2ce80aac598a3a13134bb43dbbd92d2
Aziz <aziz.khadjayev070@gmail.com>
2023-05-17 12:31:53 +0500

    Import onboarding component

--- app/onboarding/OnboardingViewScreen.jsx
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -1,5 +1,6 @@
 import { TouchableOpacity, View, Image, Text } from "react-native";
-
+import styles from "./onBoarding.style";
+import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingViewScreen = ({ handleSignIn }) => {
   return (
