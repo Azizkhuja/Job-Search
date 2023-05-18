7fe20bca8d1af47d1c0367603d8164747dcbec2f
Aziz <aziz.khadjayev070@gmail.com>
2023-05-18 10:55:05 +0500

    Add first onboarding screen

--- app/onboarding/OnboardingViewScreen.jsx
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -4,7 +4,25 @@ import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingViewScreen = ({ handleSignIn }) => {
   return (
-    
+    <>
+      <Onboarding
+        pages={[
+          {
+            backgroundColor: "#e3e3e3",
+            image: (
+              <Image
+                source={require("../../assets/images/image-4.png")}
+                style={styles.onboardingImage}
+                resizeMode="contain"
+              />
+            ),
+            title: "Something",
+            subtitle: "Done with React Native Onboarding Swiper",
+            titleStyles: { color: "red" },
+          },
+        ]}
+      />
+    </>
   );
 };
 
