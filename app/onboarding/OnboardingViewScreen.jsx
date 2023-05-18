3970c8968121d69134c321c95bb65be117a6ef43
Aziz <aziz.khadjayev070@gmail.com>
2023-05-18 10:58:35 +0500

    style sign in button

--- app/onboarding/OnboardingViewScreen.jsx
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -49,6 +49,7 @@ const OnboardingViewScreen = ({ handleSignIn }) => {
       <TouchableOpacity style={styles.googleBtn} onPress={handleSignIn}>
         <View style={styles.googleIconWrapper}>
           <Image
+            style={styles.googleIcon}
             source={{
               uri: "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png",
             }}
