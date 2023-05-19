9354438db4c043019d2848792c7a1689ebb68c90
Aziz <aziz.khadjayev070@gmail.com>
2023-05-19 23:23:31 +0500

    Remove swiper from here

--- app/onboarding/OnboardingViewScreen.jsx
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -1,51 +1,9 @@
 import { TouchableOpacity, View, Image, Text } from "react-native";
 import styles from "./onBoarding.style";
-import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingViewScreen = ({ handleSignIn }) => {
   return (
     <>
-      <Onboarding
-        pages={[
-          {
-            backgroundColor: "#e3e3e3",
-            image: (
-              <Image
-                source={require("../../assets/images/image-4.png")}
-                style={styles.onboardingImage}
-                resizeMode="contain"
-              />
-            ),
-            title: "Something",
-            subtitle: "Done with React Native Onboarding Swiper",
-            titleStyles: { color: "red" },
-          },
-          {
-            backgroundColor: "#fff",
-            image: (
-              <Image
-                source={require("../../assets/images/image-5.png")}
-                style={styles.onboardingImage}
-                resizeMode="contain"
-              />
-            ),
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
-          },
-          {
-            backgroundColor: "#fff",
-            image: (
-              <Image
-                source={require("../../assets/images/image-6.png")}
-                style={styles.onboardingImage}
-                resizeMode="contain"
-              />
-            ),
-            title: "Onboarding",
-            subtitle: "Done with React Native Onboarding Swiper",
-          },
-        ]}
-      />
       <TouchableOpacity style={styles.googleBtn} onPress={handleSignIn}>
         <View style={styles.googleIconWrapper}>
           <Image
