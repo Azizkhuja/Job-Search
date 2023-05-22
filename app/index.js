3ecf6b92841da248b5240cc6bc3c792f9e0a9b35
Aziz <aziz.khadjayev070@gmail.com>
2023-05-22 21:06:48 +0500

    Comment out

--- app/index.js
+++ app/index.js
@@ -161,7 +161,7 @@ const Home = () => {
               },
             ]}
           />
-          <OnboardingViewScreen handleSignIn={() => promptAsync()} />
+          {/* <OnboardingViewScreen handleSignIn={() => promptAsync()} /> */}
         </>
       )}
     </SafeAreaView>
