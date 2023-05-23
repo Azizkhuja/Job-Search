3c8b3f3ce8ff1accf850c2b3091deb7fb02d7469
Aziz <aziz.khadjayev070@gmail.com>
2023-05-23 18:48:55 +0500

    Add image to login

--- app/onboarding/OnboardingViewScreen.jsx
+++ app/onboarding/OnboardingViewScreen.jsx
@@ -4,6 +4,12 @@ import styles from "./onBoarding.style";
 const OnboardingViewScreen = ({ handleSignIn }) => {
   return (
     <View style={styles.onboardingWrapper}>
+      <Image
+        style={styles.googleIcon}
+        source={{
+          uri: "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png",
+        }}
+      />
       <TouchableOpacity style={styles.googleBtn} onPress={handleSignIn}>
         <View style={styles.googleIconWrapper}>
           <Image
