fde776ef5608e629930e9d261a0d65d85d763f96
Aziz <aziz.khadjayev070@gmail.com>
2023-06-08 11:14:13 +0500

    Remove comment

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -6,7 +6,6 @@ import styles from "./screenheader.style";
 const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
   const router = useRouter();
 
-  // Added the handlePress function
   const handlePress = () => {
     router.push("/profile");
   };
