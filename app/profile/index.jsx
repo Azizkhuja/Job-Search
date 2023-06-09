820f3f6f16ce16a6ba516a24cfa57bd5007ecfaf
Aziz <aziz.khadjayev070@gmail.com>
2023-06-09 10:24:20 +0500

    Add handle presser

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -1,7 +1,7 @@
 import { Text, Button } from "react-native";
 
 const ProfileInfo = () => {
-  return <Button title="Log out" />;
+  return <Button title="Log out" onPress={handleLogOut} />;
 };
 
 export default ProfileInfo;
