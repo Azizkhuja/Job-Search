e5c3429b9c15d9e5782efa597fa0de56d0ed841e
Aziz <aziz.khadjayev070@gmail.com>
2023-06-09 10:24:41 +0500

    Import router and storage

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -1,4 +1,6 @@
 import { Text, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
 
 const ProfileInfo = () => {
   return <Button title="Log out" onPress={handleLogOut} />;
