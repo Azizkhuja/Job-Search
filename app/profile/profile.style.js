86e9495d315be2f253abe715a43d3940c4d89299
Aziz <aziz.khadjayev070@gmail.com>
2023-07-15 12:38:59 +0500

    Update color

--- app/profile/profile.style.js
+++ app/profile/profile.style.js
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   profileImageContainer: {
     height: "40%",
     width: "100%",
-    backgroundColor: "#c1c1c",
+    backgroundColor: "#000",
     alignItems: "center",
   },
   imageSelf: {
