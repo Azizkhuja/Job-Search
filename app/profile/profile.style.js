106dbf73855a24ff7144643979d75058d346e1a9
Aziz <aziz.khadjayev070@gmail.com>
2023-09-18 10:47:55 +0200

    Update cokor

--- app/profile/profile.style.js
+++ app/profile/profile.style.js
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
   profileImageContainer: {
     height: "40%",
     width: "100%",
-    backgroundColor: "#000",
+    backgroundColor: "#fff",
     alignItems: "center",
   },
   imageSelf: {
